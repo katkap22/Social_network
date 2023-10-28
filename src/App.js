6f68517a0f69c77d74a3f3cc5dcd0dadc565ef11
EkaterinaKop <kat_kap@mail.ru>
2023-10-28 21:04:49 +0300

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -11,7 +11,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 
 const App = () => {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <div className="app-wrapper">
                     <Header/>
                     <Navbar />
@@ -27,7 +27,7 @@ const App = () => {
 
                     </div>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         );
     }
 ;
