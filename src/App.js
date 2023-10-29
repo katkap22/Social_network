d8bc8dd4011cd56667d2c66f20f4068afb02ee04
EkaterinaKop <kat_kap@mail.ru>
2023-10-29 22:44:34 +0300

    changed to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import React from "react";
 
 const App = () => {
         return (
-            <HashRouter>
+            <BrowserRouter>
                 <div className="app-wrapper">
                     <Header/>
                     <Navbar />
@@ -27,7 +28,7 @@ const App = () => {
 
                     </div>
                 </div>
-            </HashRouter>
+            </BrowserRouter>
         );
     }
 ;
